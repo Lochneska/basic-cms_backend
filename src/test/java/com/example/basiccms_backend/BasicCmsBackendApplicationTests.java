6e08ee7b697eaef07dacde54fe88b234fa6f5b05
Marek Lochman <lochman59@gmail.com>
2024-05-08 12:43:19 +0200

    initial commit

---
+++ src/test/java/com/example/basiccms_backend/BasicCmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basiccms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
